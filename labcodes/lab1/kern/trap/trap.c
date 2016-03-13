270493e98ea517f878070d0e35774c86f49a2b2f
高思达 <gsd13@mails.tsinghua.edu.cn>
2016-03-13 21:51:12 +0800

    写了学号。challenge 1 尚未完成。

--- labcodes/lab1/kern/trap/trap.c
+++ labcodes/lab1/kern/trap/trap.c
@@ -34,7 +34,7 @@ static struct pseudodesc idt_pd = {
 /* idt_init - initialize IDT to each of the entry points in kern/trap/vectors.S */
 void
 idt_init(void) {
-     /* LAB1 YOUR CODE : STEP 2 */
+     /* LAB1 2013011413 : STEP 2 */
      /* (1) Where are the entry addrs of each Interrupt Service Routine (ISR)?
       *     All ISR's entry addrs are stored in __vectors. where is uintptr_t __vectors[] ?
       *     __vectors[] is in kern/trap/vector.S which is produced by tools/vector.c
@@ -55,6 +55,8 @@ idt_init(void) {
 		// macro defined in memlayout.h
 		SETGATE(idt[i], 0, GD_KTEXT, __vectors[i], DPL_KERNEL);
 	}
+	// For challenge 1, need to grant the access to SWITCH_TOKernel in user mode
+	SETGATE(idt[T_SWITCH_TOK], 1, GD_KTEXT, __vectors[T_SWITCH_TOK], DPL_USER);
 	lidt(&idt_pd);
 	// My Code ends
 }
@@ -152,7 +154,7 @@ trap_dispatch(struct trapframe *tf) {
 
     switch (tf->tf_trapno) {
     case IRQ_OFFSET + IRQ_TIMER:
-        /* LAB1 YOUR CODE : STEP 3 */
+        /* LAB1 2013011413 : STEP 3 */
         /* handle the timer interrupt */
         /* (1) After a timer interrupt, you should record this event using a global variable (increase it), such as ticks in kern/driver/clock.c
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
@@ -175,6 +177,14 @@ trap_dispatch(struct trapframe *tf) {
         break;
     //LAB1 CHALLENGE 1 : YOUR CODE you should modify below codes.
     case T_SWITCH_TOU:
+		// change segment registers ro User Mode
+		tf->tf_cs = USER_CS;
+		tf->tf_ds = USER_DS;
+		tf->tf_es = USER_DS;
+		tf->tf_fs = USER_DS;
+		tf->tf_gs = USER_DS;
+		tf->tf_ss = USER_DS;
+		// 
     case T_SWITCH_TOK:
         panic("T_SWITCH_** ??\n");
         break;
