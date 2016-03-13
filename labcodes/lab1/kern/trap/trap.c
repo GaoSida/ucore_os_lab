09ef8fd64f03455f57424c62aaee6e53085152e9
高思达 <gsd13@mails.tsinghua.edu.cn>
2016-03-13 21:01:25 +0800

    完成了时钟中断的处理代码

--- labcodes/lab1/kern/trap/trap.c
+++ labcodes/lab1/kern/trap/trap.c
@@ -158,6 +158,12 @@ trap_dispatch(struct trapframe *tf) {
          * (2) Every TICK_NUM cycle, you can print some info using a funciton, such as print_ticks().
          * (3) Too Simple? Yes, I think so!
          */
+		// My code starts
+		ticks++;
+		if (ticks % TICK_NUM == 0) {
+			print_ticks();
+		}
+		// My code ends
         break;
     case IRQ_OFFSET + IRQ_COM1:
         c = cons_getc();
